7f10af950e9c3150e36205dad13e1bcf9a680577
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2024-01-01 13:10:53 -0300

    refactor: transfrming dependencies int mock Transaction.test.ts

--- test/integration/Transaction.test.ts
+++ test/integration/Transaction.test.ts
@@ -1,29 +1,59 @@
 import { CreateTransaction } from './../../src/aplication/services/Transaction/createTransaction.service';
-import { TransactionRepositoryDatabase } from './../../src/infra/repository/TransactionRepositoryDatabase';
-import { PrismaClient } from '@prisma/client';
-import { UserRepositoryDatabase } from '../../src/infra/repository/UserRepositoryDatabase';
 import { NotifyService } from '../../src/aplication/services/Notify.service';
+import { UserRepository } from '../../src/aplication/repository/UserRepository';
+import { User } from '../../src/domain/User';
+import { TransactionRepository } from '../../src/aplication/repository/TransactionRepository';
+
 describe("Should transaction", () => {
-  const client = new PrismaClient()
-
-  test("Should create new transaction", async () => {
-
-    const input = {
-      value: 100,
-      senderId: "577a603a-22df-4e6c-ae06-03ad3791e49c",
-      receiverId: "e041bea2-67e8-4323-b956-c6a8b107cb2b"
-    }
-    const repositoryUser = new UserRepositoryDatabase(client)
-    const repository = new TransactionRepositoryDatabase(client)
-    const createTransaction = new CreateTransaction(repository, repositoryUser, new NotifyService())
-    const ouput = await createTransaction.execute(input)
-    console.log(ouput)
-    const [trans] = await repository.list({
-      where: {
-        id: ouput.id
-      }
+  const userRepositoryMock: UserRepository = {
+    list: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn()
+  }
+  const transactionRepositoryMock: TransactionRepository = {
+    list: jest.fn(),
+    save: jest.fn(),
+    update: jest.fn(),
+    delete: jest.fn()
+  }
+  beforeAll(async () => {
+    const user_1 = User.create({
+      balance: 100,
+      document: "398.205.360-90",
+      email: "user@example.com",
+      firstName: "John",
+      lastName: "Smith",
+      password: "password",
+      usertype: "COMMON"
     })
-    expect(trans.id).toBeDefined()
+    const user_2 = User.create({
+      balance: 1000,
+      document: "398.205.360-90",
+      email: "user@example.com",
+      firstName: "John",
+      lastName: "Smith",
+      password: "password",
+      usertype: "COMMON"
+    })
+    jest.spyOn(userRepositoryMock, 'list').mockResolvedValue([user_1, user_2]);
+    jest.spyOn(transactionRepositoryMock, 'save')
+    jest.spyOn(transactionRepositoryMock, 'list')
   })
 
+  beforeEach(() => {
+    jest.clearAllMocks();
+  });
+  test("Should create new transaction", async () => {
+    const users = await userRepositoryMock.list();
+    const input = {
+      value: 100,
+      senderId: users[0].id,
+      receiverId: users[1].id
+    }
+    // const repository = new TransactionRepositoryDatabase(client)
+    const createTransaction = new CreateTransaction(transactionRepositoryMock, userRepositoryMock, new NotifyService())
+    const ouput = await createTransaction.execute(input)
+    expect(ouput.id).toBeDefined()
+  })
 })
