650f71eafaaa61aa02752d590198a7a945ed9d7c
Nivaldeir Silva <nivaldeir-silva@hotmail.com>
2024-01-01 13:15:10 -0300

    reafactor: remve dependese to class User para params

--- src/aplication/services/User/save.service.ts
+++ src/aplication/services/User/save.service.ts
@@ -1,9 +1,21 @@
+import { UserType } from "@prisma/client";
 import { User } from "../../../domain/User";
 import { UserRepository } from "../../repository/UserRepository";
 
 export class SaveUser {
   constructor(private readonly userService: UserRepository) { }
-  async execute(user: User): Promise<void> {
-    return this.userService.save(user)
+  async execute(user: Input): Promise<void> {
+    const newUser = User.create(user)
+    return this.userService.save(newUser)
   }
+}
+
+type Input = {
+  balance: number,
+  document: string,
+  email: string,
+  firstName: string,
+  lastName: string,
+  password: string,
+  usertype: UserType
 }
